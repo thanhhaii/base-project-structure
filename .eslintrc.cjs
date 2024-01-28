b3f3178685efd22ba3e28e22ceb8f591b8538bcc
thanhhaii <hailamnguyenthanh@gmail.com>
2024-01-28 11:32:09 +0700

    Initial Project Structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_"
+      }
+    ],
+    '@typescript-eslint/indent': ['warn', 4],
+    '@typescript-eslint/semi': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'object-curly-spacing': ['warn', 'always']
+  }
+}
